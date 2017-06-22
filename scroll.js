39be9d2295281b7fcdd9e5d523b78e330971625f
waders92 <wade.carlson@pga.com>
2017-06-22 18:11:27 -0500

    scroll speed

--- scroll.js
+++ scroll.js
@@ -12,9 +12,9 @@ $(document).ready(function(){
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
+      $('html, body').stop().animate({
         scrollTop: $(hash).offset().top
-      }, 1000, function(){
+      }, 1500, function(){
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
