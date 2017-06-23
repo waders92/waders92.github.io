2453d08c243128a1f9bc9a7778f2260191b08e20
waders92 <wade.carlson@pga.com>
2017-06-23 08:57:02 -0500

    link twist

--- fadein.js
+++ fadein.js
@@ -2,4 +2,5 @@ $(document).ready(function(){
     $("#first").delay(800).fadeIn();
     $("#second").delay(1500).fadeIn();
     $("#third").delay(2500).slideDown(2000);
+    $("#fourth").delay(5000).fadeIn("slow");
 });
