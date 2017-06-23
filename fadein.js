a1f0c4c91540949f85274a75a26870252fb0ee7b
waders92 <wade.carlson@pga.com>
2017-06-23 10:23:08 -0500

    layout

--- fadein.js
+++ fadein.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
     $("#first").delay(800).fadeIn();
     $("#second").delay(1500).fadeIn();
     $("#fourth").delay(2000).fadeIn("slow");
-    $("#third").delay(3500).slideDown(2000);
+    $("#third").delay(3000).slideDown(2000);
 });
