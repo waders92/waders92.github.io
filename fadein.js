771f18ad56cd72105574665f6a11f63f7eaa338c
waders92 <wade.carlson@pga.com>
2017-06-23 09:08:20 -0500

    link twist

--- fadein.js
+++ fadein.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#first").delay(800).fadeIn();
     $("#second").delay(1500).fadeIn();
-    $("#third").delay(2500).slideDown(2000);
-    $("#fourth").delay(5000).fadeIn("slow");
+    $("#fourth").delay(2000).fadeIn("slow");
+    $("#third").delay(3500).slideDown(2000);
 });
