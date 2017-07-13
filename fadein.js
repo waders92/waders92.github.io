d64622678e389253e8161d74c2245a87d78cd5fe
Wade Carlson <wade.carlson@pga.com>
2017-07-12 19:41:01 -0500

    Delete fadein.js

--- fadein.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("#first").delay(800).fadeIn();
-    $("#second").delay(1500).fadeIn();
-    $("#fourth").delay(2000).fadeIn("slow");
-    $("#third").delay(3000).slideDown(2000);
-});
